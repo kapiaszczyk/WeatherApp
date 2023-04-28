8b3031a0228f92d171f93c921e7dc69b8cffe3d0
kapiaszczyk <ka.piaszczyk@gmail.com>
2023-04-28 10:57:15 +0200

    Add GeocodingAPIClient to make API calls to Geocoding API

---
+++ src/main/java/WeatherApp/GeocodingAPIClient.java
@@ -0,0 +1,44 @@
+package WeatherApp;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class GeocodingAPIClient {
+
+    public String makeCall(String location) {
+
+        String url = "http://api.openweathermap.org/geo/1.0/direct?" + "q=" + location + "&limit=1" + "&appid=" + getKey();
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .build();
+        HttpResponse<String> response = null;
+
+        try {
+            response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return (response.body());
+
+    }
+
+    private String getKey() {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("src/main/java/WeatherApp/key.txt"));
+            return br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+}
