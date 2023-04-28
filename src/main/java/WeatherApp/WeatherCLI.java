a51d22e18477fc007cc9e72e3ca25261cc7b51b2
kapiaszczyk <ka.piaszczyk@gmail.com>
2023-04-28 10:30:01 +0200

    Remove redundant getWeather() method

--- src/main/java/WeatherApp/WeatherCLI.java
+++ src/main/java/WeatherApp/WeatherCLI.java
@@ -8,16 +8,7 @@ import java.util.Scanner;
 public class WeatherCLI {
 
     public void run() {
-        // getWeather();
         getWeather(getParameters());
-    }
-
-    private void getWeather() {
-        WeatherData weatherData = new WeatherData();
-        WeatherAPI weatherAPI = new WeatherAPI();
-        List<String> parameters = Arrays.asList("19.0", "58.3");
-        weatherData.getData((weatherAPI.makeCall(parameters.get(0), parameters.get(1))));
-        weatherData.printData();
     }
 
     private void getWeather(List<String> parameters) {
