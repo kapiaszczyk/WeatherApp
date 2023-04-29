97447dfea4c7579f717a35c5a676f6022b3947f0
kapiaszczyk <ka.piaszczyk@gmail.com>
2023-04-29 21:14:17 +0200

    Add fields and constructor to WeatherPresenter

--- src/main/java/WeatherApp/Presenter/WeatherPresenter.java
+++ src/main/java/WeatherApp/Presenter/WeatherPresenter.java
@@ -1,6 +1,22 @@
 package WeatherApp.Presenter;
+import WeatherApp.Geocoding.GeocodingData;
+import WeatherApp.Model.WeatherAPI;
+import WeatherApp.Model.WeatherData;
+import WeatherApp.View.WeatherAppView;
 
 public class WeatherPresenter {
+
+    private final WeatherAppView view;
+    private final WeatherData weatherData;
+    private final WeatherAPI weatherAPI;
+    private final GeocodingData geocodingData;
+
+    public WeatherPresenter() {
+        view = new WeatherAppView();
+        weatherData = new WeatherData();
+        weatherAPI = new WeatherAPI();
+        geocodingData = new GeocodingData();
+    }
 
 
 }
