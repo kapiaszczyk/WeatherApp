eeb6d73d7631eb3eb0a2e66d0ca5a21793ece7aa
kapiaszczyk <ka.piaszczyk@gmail.com>
2023-04-29 20:57:43 +0200

    Add fields with getters and setters to WeatherData class

--- src/main/java/WeatherApp/Model/WeatherData.java
+++ src/main/java/WeatherApp/Model/WeatherData.java
@@ -1,4 +1,96 @@
 package WeatherApp.Model;
 
 public class WeatherData {
+
+    private String location;
+    private String temperature;
+    private String humidity;
+    private String windSpeed;
+    private String windDirection;
+    private String pressure;
+    private String sunrise;
+    private String sunset;
+    private String clouds;
+    private String description;
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(String temperature) {
+        this.temperature = temperature;
+    }
+
+    public String getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(String humidity) {
+        this.humidity = humidity;
+    }
+
+    public String getWindSpeed() {
+        return windSpeed;
+    }
+
+    public void setWindSpeed(String windSpeed) {
+        this.windSpeed = windSpeed;
+    }
+
+    public String getWindDirection() {
+        return windDirection;
+    }
+
+    public void setWindDirection(String windDirection) {
+        this.windDirection = windDirection;
+    }
+
+    public String getPressure() {
+        return pressure;
+    }
+
+    public void setPressure(String pressure) {
+        this.pressure = pressure;
+    }
+
+    public String getSunrise() {
+        return sunrise;
+    }
+
+    public void setSunrise(String sunrise) {
+        this.sunrise = sunrise;
+    }
+
+    public String getSunset() {
+        return sunset;
+    }
+
+    public void setSunset(String sunset) {
+        this.sunset = sunset;
+    }
+
+    public String getClouds() {
+        return clouds;
+    }
+
+    public void setClouds(String clouds) {
+        this.clouds = clouds;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
 }
