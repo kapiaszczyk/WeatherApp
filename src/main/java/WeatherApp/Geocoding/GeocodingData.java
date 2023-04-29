71c1126c28e207f0643a8bb9668f95e0b0e9f5f2
kapiaszczyk <ka.piaszczyk@gmail.com>
2023-04-29 20:46:14 +0200

    Add verifyLocation method to verify input location

--- src/main/java/WeatherApp/Geocoding/GeocodingData.java
+++ src/main/java/WeatherApp/Geocoding/GeocodingData.java
@@ -30,6 +30,11 @@ public class GeocodingData {
     }
 
     public void getCoordinates(String locationInput) {
+        try {
+            verifyLocation(locationInput);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
         GeocodingAPIClient geocodingAPIClient = new GeocodingAPIClient();
         GeocodingResponseParser geocodingResponseParser = new GeocodingResponseParser();
         geocodingResponseParser.getData(geocodingAPIClient.makeCall(locationInput));
@@ -37,4 +42,11 @@ public class GeocodingData {
         longitude = geocodingResponseParser.getLon();
         location = geocodingResponseParser.getPlaceName();
     }
+
+    private void verifyLocation(String locationInput) {
+        if (locationInput == null || locationInput.isEmpty() || locationInput.trim().isEmpty()) {
+            throw new IllegalArgumentException("Location cannot be empty");
+        }
+    }
+
 }
