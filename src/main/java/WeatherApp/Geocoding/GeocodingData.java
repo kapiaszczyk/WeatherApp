01da7098d8433430d33a90641b53f33136c05362
kapiaszczyk <ka.piaszczyk@gmail.com>
2023-04-29 20:43:54 +0200

    Add GeocodingData to store reply from Geocoding API

---
+++ src/main/java/WeatherApp/Geocoding/GeocodingData.java
@@ -0,0 +1,32 @@
+package WeatherApp.Geocoding;
+
+public class GeocodingData {
+    String latitude;
+    String longitude;
+    String location;
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+}
