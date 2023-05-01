e5d663c84442dc1a90f73a99a330a818117696e1
kapiaszczyk <ka.piaszczyk@gmail.com>
2023-05-01 11:32:32 +0200

    Remove formatting mistakes in printWeatherData method

--- src/main/java/WeatherApp/View/WeatherAppView.java
+++ src/main/java/WeatherApp/View/WeatherAppView.java
@@ -22,11 +22,11 @@ public class WeatherAppView {
                                  String sunset) {
 
         System.out.println("Weather for " + location + " at " + LocalTime.now() + " local time: " + description);
-        System.out.println("Temperature: " + temperature + "Celsius degrees");
+        System.out.println("Temperature: " + temperature + " Celsius degrees");
         System.out.println("Humidity: " + humidity + "%");
         System.out.println("Pressure: " + pressure + " hPa");
         System.out.println("Wind Speed: " + windSpeed + " m/s");
-        System.out.println("Wind Direction: " + windDirection + "degrees");
+        System.out.println("Wind Direction: " + windDirection + " degrees");
         System.out.println("Clouds: " + clouds + "%");
         System.out.println("Sunrise: " + sunrise + " local time");
         System.out.println("Sunset: " + sunset + " local time");
