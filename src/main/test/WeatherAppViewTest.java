49f30dba42ebd8e86086ed5cb8bc2b7bc5b086c3
kapiaszczyk <ka.piaszczyk@gmail.com>
2023-05-01 10:59:40 +0200

    Add unit tests for getWeather method of WeatherPresenter class

---
+++ src/main/test/WeatherAppViewTest.java
@@ -0,0 +1,31 @@
+import WeatherApp.Presenter.WeatherPresenter;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+public class WeatherAppViewTest {
+
+    @Test
+    public void testNullLocationGetWeather() {
+        WeatherPresenter presenter = new WeatherPresenter();
+        Assert.assertThrows(IllegalArgumentException.class, () -> {
+            presenter.getWeather(null);
+        });
+    }
+
+    @Test
+    public void testEmptyLocationGetWeather() {
+        WeatherPresenter presenter = new WeatherPresenter();
+        Assert.assertThrows(IllegalArgumentException.class, () -> {
+            presenter.getWeather("");
+        });
+    }
+
+    @Test
+    public void testBlankLocationGetWeather() {
+        WeatherPresenter presenter = new WeatherPresenter();
+        Assert.assertThrows(IllegalArgumentException.class, () -> {
+            presenter.getWeather(" ");
+        });
+    }
+
+}
