2ae026c50b689d11985e6046505ad29e571b05ed
kapiaszczyk <ka.piaszczyk@gmail.com>
2023-05-01 20:00:51 +0200

    Remove unused import statements

--- src/main/test/WeatherApp/Weather/WeatherResponseParserTest.java
+++ src/main/test/WeatherApp/Weather/WeatherResponseParserTest.java
@@ -3,9 +3,6 @@ package WeatherApp.Weather;
 import WeatherApp.WeatherAPI.WeatherResponseParser;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
