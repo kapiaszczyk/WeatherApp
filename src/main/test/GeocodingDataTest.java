24197c5669785b64fb05d0ce0fb338cbc4b3ed12
kapiaszczyk <ka.piaszczyk@gmail.com>
2023-05-01 13:57:54 +0200

    Add unit tests for getCoordinates method

---
+++ src/main/test/GeocodingDataTest.java
@@ -0,0 +1,16 @@
+import WeatherApp.Geocoding.GeocodingData;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GeocodingDataTest {
+
+    @Test
+    public void testGetCoordinates() {
+        GeocodingData geocodingData = new GeocodingData();
+        geocodingData.getCoordinates("New York");
+        assertEquals("40.7127281", geocodingData.getLatitude());
+        assertEquals("-74.0060152", geocodingData.getLongitude());
+        assertEquals("New York County", geocodingData.getLocation());
+    }
+
+}
