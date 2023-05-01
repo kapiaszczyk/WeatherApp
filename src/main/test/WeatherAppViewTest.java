bb2330c0c248f603cf1bd038b470a363bfba0002
kapiaszczyk <ka.piaszczyk@gmail.com>
2023-05-01 11:11:00 +0200

    Change lambda statement with lambda expressions

--- src/main/test/WeatherAppViewTest.java
+++ src/main/test/WeatherAppViewTest.java
@@ -7,33 +7,25 @@ public class WeatherAppViewTest {
     @Test
     public void testNullLocationGetWeather() {
         WeatherPresenter presenter = new WeatherPresenter();
-        Assert.assertThrows(IllegalArgumentException.class, () -> {
-            presenter.getWeather(null);
-        });
+        Assert.assertThrows(IllegalArgumentException.class, () -> presenter.getWeather(null));
     }
 
     @Test
     public void testEmptyLocationGetWeather() {
         WeatherPresenter presenter = new WeatherPresenter();
-        Assert.assertThrows(IllegalArgumentException.class, () -> {
-            presenter.getWeather("");
-        });
+        Assert.assertThrows(IllegalArgumentException.class, () -> presenter.getWeather(""));
     }
 
     @Test
     public void testBlankLocationGetWeather() {
         WeatherPresenter presenter = new WeatherPresenter();
-        Assert.assertThrows(IllegalArgumentException.class, () -> {
-            presenter.getWeather(" ");
-        });
+        Assert.assertThrows(IllegalArgumentException.class, () -> presenter.getWeather(" "));
     }
 
     @Test
     public void testNonWordLocationGetWeather() {
         WeatherPresenter presenter = new WeatherPresenter();
-        Assert.assertThrows(IllegalArgumentException.class, () -> {
-            presenter.getWeather("123");
-        });
+        Assert.assertThrows(IllegalArgumentException.class, () -> presenter.getWeather("123"));
     }
 
 }
