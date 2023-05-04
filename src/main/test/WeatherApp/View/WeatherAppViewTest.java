1f5bd2709fc4afcd2ed554f870987f53ecce7bdc
kapiaszczyk <ka.piaszczyk@gmail.com>
2023-05-04 22:55:27 +0200

    Remove test relating to WeatherPresenter placed in the incorrect test class WeatherAppViewTest

--- src/main/test/WeatherApp/View/WeatherAppViewTest.java
+++ src/main/test/WeatherApp/View/WeatherAppViewTest.java
@@ -1,37 +1,7 @@
 package WeatherApp.View;
 
-import WeatherApp.Presenter.WeatherPresenter;
-import org.junit.Assert;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
 public class WeatherAppViewTest {
 
-    WeatherPresenter presenter;
 
-    @BeforeEach
-    public void setUp() {
-        presenter = new WeatherPresenter();
-    }
-
-    @Test
-    public void testNullLocationGetWeather() {
-        Assert.assertThrows(IllegalArgumentException.class, () -> presenter.getWeather(null));
-    }
-
-    @Test
-    public void testEmptyLocationGetWeather() {
-        Assert.assertThrows(IllegalArgumentException.class, () -> presenter.getWeather(""));
-    }
-
-    @Test
-    public void testBlankLocationGetWeather() {
-        Assert.assertThrows(IllegalArgumentException.class, () -> presenter.getWeather(" "));
-    }
-
-    @Test
-    public void testNonWordLocationGetWeather() {
-        Assert.assertThrows(IllegalArgumentException.class, () -> presenter.getWeather("123"));
-    }
 
 }
