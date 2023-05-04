7540715483def5e30502446e8d8ee1fea348c4e3
kapiaszczyk <ka.piaszczyk@gmail.com>
2023-05-04 22:40:27 +0200

    Add unit test testNullGetWeather to test getWeather method against passing null arguments

--- src/main/test/WeatherApp/Presenter/WeatherPresenterTest.java
+++ src/main/test/WeatherApp/Presenter/WeatherPresenterTest.java
@@ -1,4 +1,13 @@
 package WeatherApp.Presenter;
 
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
 public class WeatherPresenterTest {
+
+    @Test
+    public void testNullGetWeather() {
+        WeatherPresenter presenter = new WeatherPresenter();
+        Assert.assertThrows(IllegalArgumentException.class, () -> presenter.getWeather(null));
+    }
 }
