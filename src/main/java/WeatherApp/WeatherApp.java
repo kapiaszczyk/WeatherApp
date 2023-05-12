0f85dc964c4c43af5deef96a8639bae6174cb8b5
kapiaszczyk <ka.piaszczyk@gmail.com>
2023-05-13 01:37:06 +0200

    Add command line argument parsing to the main method

--- src/main/java/WeatherApp/WeatherApp.java
+++ src/main/java/WeatherApp/WeatherApp.java
@@ -1,10 +1,14 @@
 package WeatherApp;
 
 import WeatherApp.Presenter.WeatherPresenter;
+import WeatherApp.Util.ArgumentParser;
+
 public class WeatherApp {
     public static void main (String[] args) {
+
+        ArgumentParser parser = new ArgumentParser();
         WeatherPresenter presenter = new WeatherPresenter();
-        presenter.getWeather();
+        presenter.getWeather(parser.parse(args));
         presenter.showWeather();
     }
 }
