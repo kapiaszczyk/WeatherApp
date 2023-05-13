ad7e4e6400e6be19cf9794ccb9f14e0df38a79c5
kapiaszczyk <ka.piaszczyk@gmail.com>
2023-05-13 13:55:38 +0200

    Add WeatherDataFileWriter class to handle writing OpenWeather API response to file

---
+++ src/main/java/WeatherApp/Util/WeatherDataFileWriter.java
@@ -0,0 +1,17 @@
+package WeatherApp.Util;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class WeatherDataFileWriter {
+
+    public void writeToFile(String data, String fileName) {
+        try {
+            FileWriter fileWriter = new FileWriter(fileName + ".JSON");
+            fileWriter.write(data);
+            fileWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
