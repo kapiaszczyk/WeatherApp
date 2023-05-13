bcb045c7b0e58a3aa9cbd4f334a925557ce971c6
kapiaszczyk <ka.piaszczyk@gmail.com>
2023-05-13 13:55:04 +0200

    Remove printWeatherDataToFile method from WeatherData (SRP)

--- src/main/java/WeatherApp/WeatherAPI/WeatherData.java
+++ src/main/java/WeatherApp/WeatherAPI/WeatherData.java
@@ -124,10 +124,4 @@ public class WeatherData {
         return convertedTime.toString();
     }
 
-    public void printWeatherDataToFile(String fileName) {
-        WeatherResponseParser weatherDataParser = new WeatherResponseParser();
-        weatherDataParser.getData(this.response);
-        weatherDataParser.printResponse(this.response, fileName);
-    }
-
 }
