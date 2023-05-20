fe58ee7c300357874de2d7fd6420f9eea3bdd84f
kapiaszczyk <ka.piaszczyk@gmail.com>
2023-05-21 00:12:38 +0200

    Removed unused imports

--- src/main/test/WeatherApp/Presenter/WeatherPresenterTest.java
+++ src/main/test/WeatherApp/Presenter/WeatherPresenterTest.java
@@ -1,37 +1,38 @@
 package WeatherApp.Presenter;
 
-import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class WeatherPresenterTest {
 
     @Test
     public void testNullGetWeather() {
         WeatherPresenter presenter = new WeatherPresenter();
-        Assert.assertThrows(IllegalArgumentException.class, () -> presenter.getWeather((String) null));
+        assertThrows(IllegalArgumentException.class, () -> presenter.getWeather((String) null));
     }
 
     @Test
     public void testEmptyGetWeather() {
         WeatherPresenter presenter = new WeatherPresenter();
-        Assert.assertThrows(IllegalArgumentException.class, () -> presenter.getWeather(""));
+        assertThrows(IllegalArgumentException.class, () -> presenter.getWeather(""));
     }
 
     @Test
     public void testBlankGetWeather() {
         WeatherPresenter presenter = new WeatherPresenter();
-        Assert.assertThrows(IllegalArgumentException.class, () -> presenter.getWeather(" "));
+        assertThrows(IllegalArgumentException.class, () -> presenter.getWeather(" "));
     }
 
     @Test
     public void testNonWordGetWeather() {
         WeatherPresenter presenter = new WeatherPresenter();
-        Assert.assertThrows(IllegalArgumentException.class, () -> presenter.getWeather("123"));
+        assertThrows(IllegalArgumentException.class, () -> presenter.getWeather("123"));
     }
 
     @Test
     public void testNullShowWeather() {
         WeatherPresenter presenter = new WeatherPresenter();
-        Assert.assertThrows(NullPointerException.class, () -> presenter.showWeather());
+        assertThrows(NullPointerException.class, () -> presenter.showWeather());
     }
 }
--- src/main/test/WeatherApp/View/WeatherAppViewTest.java
+++ src/main/test/WeatherApp/View/WeatherAppViewTest.java
@@ -1,20 +1,21 @@
 package WeatherApp.View;
 
-import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class WeatherAppViewTest {
 
     @Test
     public void testAllNullPrintWeatherData() {
         WeatherAppView view = new WeatherAppView();
-        Assert.assertThrows(NullPointerException.class, () -> view.printWeatherData(null, null, null, null, null, null, null, null, null, null));
+        assertThrows(NullPointerException.class, () -> view.printWeatherData(null, null, null, null, null, null, null, null, null, null));
     }
 
     @Test
     public void testNullLocationPrintWeatherData() {
         WeatherAppView view = new WeatherAppView();
-        Assert.assertThrows(NullPointerException.class, () -> view.printWeatherData(null, "description", "temperature", "humidity", "pressure", "windSpeed", "windDirection", "clouds", "sunrise", "sunset"));
+        assertThrows(NullPointerException.class, () -> view.printWeatherData(null, "description", "temperature", "humidity", "pressure", "windSpeed", "windDirection", "clouds", "sunrise", "sunset"));
     }
 
 }
